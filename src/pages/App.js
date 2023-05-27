44b24a37e85ae2ec93e28d182a8b1cabacd23aeb
Abdul Basit Mehtab <abasitm.1993@gmail.com>
2023-05-28 00:40:22 +0500

    Added update todo functionality

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,24 +1,33 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import { BASE_URL } from "../constants/apiURL";
-import { getCall, postCall, deleteCall } from "../services/apiService";
+import { getCall, postCall, deleteCall, putCall } from "../services/apiService";
 import LoadingSpinner from "../components/LoadingSpinner/LoadingSpinner";
 import ListItem from "../components/ListItem/ListItem";
 import InputBox from "../components/InputBox/InputBox";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Modal from "../components/Modal/Modal";
+import moment from "moment";
 
 const App = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [todos, setTodos] = useState([]);
 
   const [todoText, setTodoText] = useState("");
+  const [updateTodoText, setUpdateTodoText] = useState("");
   const [selectedTodo, setSelectedTodo] = useState(null);
+  const [selectedEditTodo, setSelectedEditTodo] = useState(null);
   const [showModal, setShowModal] = useState(false);
+  const [showEditModal, setShowEditModal] = useState(false);
+  const [isViewMode, setIsViewMode] = useState(false);
 
   const onChangeTodoText = (e) => {
     setTodoText(e.target.value);
+  };
+
+  const onChangeUpdateText = (e) => {
+    setUpdateTodoText(e.target.value);
   };
 
   const handleSubmit = async () => {
@@ -72,6 +81,12 @@ const App = () => {
     setSelectedTodo(item);
   };
 
+  const handleEditClick = (item) => {
+    console.log("item to update...", item);
+    setShowEditModal(true);
+    setSelectedEditTodo(item);
+  };
+
   const handleDeleteTodo = async () => {
     setShowModal(false);
     setIsLoading(true);
@@ -90,6 +105,34 @@ const App = () => {
           setIsLoading(false);
           toast(err?.message, { type: "error" });
         });
+    }
+  };
+
+  const handleUpdateTodo = async () => {
+    setIsLoading(true);
+
+    let data = {
+      content: updateTodoText,
+    };
+
+    if (updateTodoText || !updateTodoText == "") {
+      await putCall(`${BASE_URL}/todos/update/${selectedEditTodo?._id}`, data)
+        .then((res) => {
+          setIsLoading(false);
+          console.log("response data update todo ==>> ", res);
+          toast(res?.data?.message, { type: "success" });
+          setUpdateTodoText("");
+          setShowEditModal(!showEditModal);
+          getData();
+        })
+        .catch((err) => {
+          console.log(err);
+          setIsLoading(false);
+          toast(err?.message, { type: "error" });
+        });
+    } else {
+      setIsLoading(false);
+      return toast("Todo is empty! Please enter some text", { type: "info" });
     }
   };
 
@@ -121,6 +164,7 @@ const App = () => {
               key={item?._id}
               item={item}
               onDeleteClick={() => handleDeleteClick(item)}
+              onEditClick={() => handleEditClick(item)}
             />
           );
         })
@@ -169,6 +213,99 @@ const App = () => {
               No
             </button>
           </div>
+        </div>
+      </Modal>
+
+      <Modal
+        show={showEditModal}
+        title="Update Todo"
+        onClose={() => setShowEditModal(false)}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          {isViewMode ? (
+            <>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <h3 style={{ margin: 0, padding: 0 }}>Todo Created At:</h3>
+                <p style={{ textAlign: "center" }}>
+                  {moment(selectedEditTodo?.createdAt).format(
+                    "dddd, MMMM D, YYYY h:mm A"
+                  )}
+                </p>
+                <p style={{ margin: 0, padding: 0 }}>
+                  ({moment(selectedEditTodo?.createdAt).fromNow()})
+                </p>
+
+                <div style={{ marginTop: 30 }}></div>
+
+                <h3 style={{ margin: 0, padding: 0 }}>Todo Updated At:</h3>
+                <p style={{ textAlign: "center" }}>
+                  {moment(selectedEditTodo?.updatedAt).format(
+                    "dddd, MMMM D, YYYY h:mm A"
+                  )}
+                </p>
+                <p style={{ margin: 0, padding: 0 }}>
+                  ({moment(selectedEditTodo?.updatedAt).fromNow()})
+                </p>
+              </div>
+
+              <div onClick={() => setIsViewMode(!isViewMode)}>
+                <h5 className="todoDetailLink">Go back</h5>
+              </div>
+            </>
+          ) : (
+            <>
+              <input
+                type="text"
+                placeholder={selectedEditTodo?.content}
+                className="updateInputBox"
+                onChange={onChangeUpdateText}
+                // value={value}
+              />
+
+              <div onClick={() => setIsViewMode(!isViewMode)}>
+                <h5 className="todoDetailLink">View Todo Details</h5>
+              </div>
+
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                  justifyContent: "space-around",
+                }}
+              >
+                <button
+                  className="submitBtn"
+                  onClick={() => handleUpdateTodo()}
+                >
+                  Submit
+                </button>
+
+                <button
+                  className="noBtn"
+                  onClick={() => {
+                    setShowEditModal(!showEditModal);
+                  }}
+                >
+                  Cancel
+                </button>
+              </div>
+            </>
+          )}
         </div>
       </Modal>
 
